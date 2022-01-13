91918fd356cccc34cee7ed5452da63c44049cd8f
haaamed-30 <haaamed_zo6@hotmail.com>
2022-01-13 19:12:56 +0300

    add skills event

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,17 +1,16 @@
+// app for counting numbers at page
 let nums = document.querySelectorAll('#nums .number');
 let stats = document.querySelector('#statsNums');
 let started = false; //Function not started
 
-
-
-window.onscroll = function () {
+document.addEventListener("scroll", () => {
     if (window.scrollY >= stats.offsetTop) {
         if (!started) {
             nums.forEach((num) => startCount(num));
         }
         started = true;
     }
-};
+});
 
 function startCount(el) {
     let goal = el.dataset.goal;
@@ -22,4 +21,20 @@ function startCount(el) {
         }
     }, 2000 / goal);
 }
+// app for counting numbers at page
+// To laying skills width
+let section = document.querySelector(".skills");
+let spanSkills = document.querySelectorAll(".skills span");
+
+function skillsSpans() {
+    if (window.scrollY >= section.offsetTop - 300) {
+        spanSkills.forEach((span) => {
+            span.style.width = span.dataset.progress;
+        })
+    }
+}
+document.addEventListener("scroll", () => {
+    skillsSpans();
+});
+// To laying skills width
 
